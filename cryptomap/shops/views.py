56c5566bb26054458ecd5c7a84e3e45871a0ca1c
ruslanofc <ruslanofcourse@gmail.com>
2021-03-19 12:54:54 +0300

    добавил кастомный тег get_categories, отвечает за вывод категорий магазинов

--- cryptomap/shops/views.py
+++ cryptomap/shops/views.py
@@ -7,25 +7,19 @@ from products.models import *
 
 def index(request):
     shopsDescriptions = ShopDescription.objects.all()
-    categories = Category.objects.all()
-    # products = ProductDescription.objects.all()
     context = {
         'shopsDescriptions': shopsDescriptions,
         'title': 'Список магазинов',
-        # 'products': products,
-        'categories': categories
     }
     return render(request, template_name='shops/index.html', context=context)
 
 
 def get_category(request, category_id):
     shopsDescriptions = ShopDescription.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
 
     context = {
         'shopsDescriptions': shopsDescriptions,
-        'categories': categories,
         'category': category
     }
 
