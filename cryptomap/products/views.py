86864b487157f608cdb61f341438e5001c2adde7
ruslanofc <ruslanofcourse@gmail.com>
2021-05-19 14:15:33 +0300

    рефакторинг товаров, выво всех товаров, вывод товаров по категориям

--- cryptomap/products/views.py
+++ cryptomap/products/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .forms import *
 from .models import *
 from django.core.paginator import Paginator
+from django.views.generic.list import ListView
 
 
 def view_product(request, product_id):
@@ -12,34 +13,50 @@ def view_product(request, product_id):
     return render(request, 'products/view_product.html', {"products": products, "products_descriptions": products_descriptions})
 
 
-# def view_all_products(request):
+class ProductsView(ListView):
+
+    model = Product
+    queryset = Product.objects.all()
+    paginate_by = 2
+
+
+class ProductsByCategory(ListView):
+    model = ProductDescription
+    template_name = 'products/all_products_by_category.html'
+    context_object_name = 'products'
+    paginate_by = 2
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = ProductDescription.objects.filter(pk=self.kwargs['category_id'])
+        context['shopsDescriptions'] = ShopDescription.objects.all()
+        return context
+
+    def get_queryset(self):
+        return ProductDescription.objects.filter(category_id=self.kwargs['category_id'])
+
+
+# def get_products_by_category(request, category_id):
+#     # для вывода информации о продукте
+#     productsDescriptions = ProductDescription.objects.filter(category_id=category_id)
+#
+#     # для вывода тайтла
+#     category = ProductCategory.objects.get(pk=category_id)
+#
+#     # контекст для кнопки перехода в магазин
+#     shopsDescriptions = ShopDescription.objects.all()
+#
+#     paginator = Paginator(productsDescriptions, 2)
+#     page_num = request.GET.get('page', 1)
+#     page_objects = paginator.get_page(page_num)
 #     context = {
-#         'title': 'Все товары',
+#         # 'productsDescriptions': productsDescriptions,
+#         'page_obj': page_objects,
+#         'category': category,
+#         'shopsDescriptions': shopsDescriptions
 #     }
+#
 #     return render(request, template_name='products/all_products_by_category.html', context=context)
-
-
-def get_products_by_category(request, category_id):
-    # для вывода информации о продукте
-    productsDescriptions = ProductDescription.objects.filter(category_id=category_id)
-
-    # для вывода тайтла
-    category = ProductCategory.objects.get(pk=category_id)
-
-    # контекст для кнопки перехода в магазин
-    shopsDescriptions = ShopDescription.objects.all()
-
-    paginator = Paginator(productsDescriptions, 2)
-    page_num = request.GET.get('page', 1)
-    page_objects = paginator.get_page(page_num)
-    context = {
-        # 'productsDescriptions': productsDescriptions,
-        'page_obj': page_objects,
-        'category': category,
-        'shopsDescriptions': shopsDescriptions
-    }
-
-    return render(request, template_name='products/all_products_by_category.html', context=context)
 
 
 def add_product(request, shop_id):
